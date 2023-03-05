a1be7da4a9f65d02671a809cf0ea822165c44d10
José Ferreira Gomes <jose-ferreira.gomes@capgemini.com>
2023-03-05 11:28:27 +0000

    upload database

---
+++ LoanManagementDB/dbo/Stored Procedures/SpGetPageOfCustomerInstalments.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE SpGetPageOfCustomerInstalments
+	@CustomerId int
+	, @PageSize int=2
+	,@LastPageLastInstallmentId int=0
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    SELECT cust.CustomerId, cust.CustomerName, ln.LoanDescription, inst.Installment, inst.InstallmentId
+	FROM Installments inst RIGHT JOIN Loans ln ON inst.LoanId = ln.LoanId RIGHT JOIN Customers cust ON ln.Customer_CustomerId=cust.CustomerId
+	WHERE cust.CustomerId=@CustomerId
+	AND inst.InstallmentId > @LastPageLastInstallmentId
+	ORDER BY inst.InstallmentId OFFSET 0 ROWS FETCH NEXT 2 ROWS ONLY
+END
---
+++ LoanManagementDB/dbo/Tables/Installments.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Installments] (
+    [InstallmentId] INT             IDENTITY (1, 1) NOT NULL,
+    [LoanId]        INT             NOT NULL,
+    [Installment]   DECIMAL (15, 5) NOT NULL,
+    CONSTRAINT [FK_Installments_Loans] FOREIGN KEY ([LoanId]) REFERENCES [dbo].[Loans] ([LoanId])
+);
+
