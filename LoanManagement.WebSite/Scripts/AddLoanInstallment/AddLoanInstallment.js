a585047a734acd8ccd7aea8fc15ec9f1e426ad94
José Ferreira Gomes <jose-ferreira.gomes@capgemini.com>
2023-03-18 14:49:02 +0000

    observable pattern javascript

--- LoanManagement.WebSite/Scripts/AddLoanInstallment/AddLoanInstallment.js
+++ LoanManagement.WebSite/Scripts/AddLoanInstallment/AddLoanInstallment.js
@@ -1,61 +1,114 @@
-﻿ 
-var ObservableUIElem;
-
-////////////////////////////////
+﻿////////////////////////////////
 //Observer library declaration//
 ////////////////////////////////
 
 ////////////////////////////////
-//Observer         declaration//
+//Observer prototype declaration//
 ////////////////////////////////
 function ObserverObj(elemId) {
     console.log("observer created " + elemId);
     this.elementId = elemId;
-}
-ObserverObj.prototype.notify = function () {
-    console.log("observer notify")
+    this.notify = (function (elemVal) {
+        console.log("observer notify")
+        $("#" + this.elementId).val("1 " + elemVal);
+    }).bind(this);
 }
 
 ////////////////////////////////
-//Observable       declaration//
+//Observable prototype declaration//
 ////////////////////////////////
-function ObservableObj(elemId) {
+function ObservableObj(elemId) {    
     console.log("observable created " + elemId);
     this.elementId = elemId;
     this.observerList = new Array();
+    this.notifyObservers = (function () {
+        
+        for (var i = 0; i < this.observerList.length; i++) {
+            this.observerList[i].notify($("#" + this.elementId).val());
+        }
+        console.log("notify observers");
+    }).bind(this);
+    this.addObserver = (function (observerObj) {
+        this.observerList.push(observerObj);
+        console.log("observer added: " + observerObj.elementId);
+    }).bind(this);
+    this.AttatchFocusOut = (function () {
+        $("#" + this.elementId).focusout(this.notifyObservers);
+    }).bind(this);
 }
 
-ObservableObj.prototype.addObserver = function (observerObj) {
-    this.observerList.push(observerObj);
+////////////////////////////////
+//Observer class declaration//
+////////////////////////////////
+class ObserverClass {
 
-    console.log("observer added: " + observerObj.elementId);
-};
-
-ObservableObj.prototype.notifyObservers = function (elem) {
-
-    for (var i = 0; i < elem.observerList.length; i++) {
-        elem.observerList[i].notify();
+    constructor(elemId) {
+        this.elementId = elemId;
     }
-    console.log("notify observers");
-};
-ObservableObj.prototype.AttatchFocusOut = function () {
-    $("#" + this.elementId).focusout(this.notifyObservers(this));
-};
+    notify = (function (elemVal) {
+        console.log("class observer notify")
+        $("#" + this.elementId).val("1 " + elemVal);
+    }).bind(this)
+}
+
+////////////////////////////////
+//Observable class declaration//
+////////////////////////////////
+class ObservableClass{
+    constructor(elemId) {
+        this.elementId = elemId;
+        this.observerList = new Array();
+    }
+    notifyObservers = (function() {
+        for (var i = 0; i < this.observerList.length; i++) {
+            this.observerList[i].notify($("#" + this.elementId).val());
+        }
+        console.log("notify observers");
+    }).bind(this)
+    addObserver = (function (observerObj) {
+        this.observerList.push(observerObj);
+        console.log("observer added: " + observerObj.elementId);
+    }).bind(this)
+    AttatchFocusOut = (function () {
+        $("#" + this.elementId).focusout(this.notifyObservers);
+    }).bind(this)
+}
+
 $(document).ready(function () {           
-    
+
+    ////using prototype
+    ////observer pattern setup
+    ////add observables considering their id's
+    //var ObservableUIElem = new ObservableObj("CustomerNameTextBox");
+    //var ObservableUIElem2 = new ObservableObj("LoanDescriptionTextBox");
+
+    ////add observers considering their id's
+    //var observerElem = new ObserverObj("LoanDescriptionTextBox");
+    //var observerElem2 = new ObserverObj("InstallmentValueTextBox");
+
+    ////add observers to observables
+    //ObservableUIElem.addObserver(observerElem);
+    //ObservableUIElem2.addObserver(observerElem2);
+    ////attatch focusout
+    //ObservableUIElem.AttatchFocusOut();
+    //ObservableUIElem2.AttatchFocusOut();
+
+    //using classes
     //observer pattern setup
     //add observables considering their id's
-    ObservableUIElem = new ObservableObj("CustomerNameTextBox");
+    var ObservableUIElem = new ObservableClass("CustomerNameTextBox");
+    var ObservableUIElem2 = new ObservableClass("LoanDescriptionTextBox");
 
     //add observers considering their id's
-    observerElem = new ObserverObj("LoanDescriptionTextBox");
+    var observerElem = new ObserverClass("LoanDescriptionTextBox");
+    var observerElem2 = new ObserverClass("InstallmentValueTextBox");
 
     //add observers to observables
     ObservableUIElem.addObserver(observerElem);
-
+    ObservableUIElem2.addObserver(observerElem2);
     //attatch focusout
     ObservableUIElem.AttatchFocusOut();
-   
+    ObservableUIElem2.AttatchFocusOut();
 
     //legacy attach
     //$("#CustomerNameTextBox").focusout(function (elm) {
